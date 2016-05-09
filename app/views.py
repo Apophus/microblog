ea85d4e80e2af98217df3006132ff3d22e72ebf4
Apophus <bkilel12@gmail.com>
2016-05-09 10:13:03 +0300

    M0dified the view to greet user

--- app/views.py
+++ app/views.py
@@ -6,6 +6,9 @@ Each is mapped to one or more **REQUEST URLs**
 
     The view we are creating returns a string to be displayed on the client's web browser
 '''
+from flask import render_template
+'''The above imported function takes a template file name and a variable list of template arguments and returns the rendered template with arguments replaced.
+It also invokes the Jinja2 templating engine. Jinja2 replaces the{{..}} blocks with the corresponding values for template arguments.'''
 from app import app
 '''Essentially, decorators work as wrappers, modifying the behavior of the code before and after a target function execution.
 
@@ -14,4 +17,7 @@ These decorators create mappings from _URLs_ / and _/index_ to it's function
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
