15cef54c960ef1cf7927d625a52dff697fa9626c
Apophus <bkilel12@gmail.com>
2016-05-15 23:40:52 +0300

    updated config.py

--- config.py
+++ config.py
@@ -1,16 +1,6 @@
 #!usr/bin/python
-'''
-This is the configuration settings.
-Flask WTF extension wraps the WTForms in a way that intergrate nicely with flask app.
-**WTF_CSFR_ENABLED** activates cross-site forgery request prevention which makes the site more secure.
-SECRET_KEY is only needed where CSFR  is enabled. It provides a cryptographic token used to validate web forms. 
-'''
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
-    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
-    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
-    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')#path to the database
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')#folder where SQLAlchemy-migrate files are stored.
